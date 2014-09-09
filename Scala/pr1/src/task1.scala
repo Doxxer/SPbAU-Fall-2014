7dfe610b1f540b96103d68bfef88dfc600ab05dd
Timur <doxxerACM@gmail.com>
2014-09-09 12:42:54 +0400

    scala pr1 initial commit

---
+++ Scala/pr1/src/task1.scala
@@ -0,0 +1,20 @@
+import scala.annotation.tailrec
+
+/**
+ * GCD calculus
+ *
+ * Created by Turaev Timur on 09.09.14.
+ */
+object task1 {
+  @tailrec
+  def gcd(x: Long, y: Long): Long = {
+    if (y == 0) x else gcd(y, x % y)
+  }
+
+  def main(args: Array[String]) {
+    println(gcd(10, 5))
+    println(gcd(10, 3))
+    println(gcd(10, 0))
+    println(gcd(0, 10))
+  }
+}
---
+++ Scala/pr1/src/task2.scala
@@ -0,0 +1,9 @@
+/**
+ * Fibonacci numbers
+ * Created by Turaev Timur on 09.09.14.
+ */
+object task2 {
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
