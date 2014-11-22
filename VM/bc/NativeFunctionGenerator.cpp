03503cf524b53f4eb166de6a80e2cfe97882e440
Timur <doxxerACM@gmail.com>
2014-11-23 00:13:01 +0300

    vm bc native cont

--- VM/bc/NativeFunctionGenerator.cpp
+++ VM/bc/NativeFunctionGenerator.cpp
@@ -1,6 +1,7 @@
 #include "SimpleInterpreter.hpp"
 #include "asmjit.h"
 #include "Errors.hpp"
+#include "AsmVariable.hpp"
 
 using namespace asmjit;
 
@@ -14,41 +15,64 @@ namespace mathvm {
         }
 
         JitRuntime runtime;
-        FileLogger logger(stdout);
         X86Compiler compiler(&runtime);
-        compiler.setLogger(&logger);
-
         switch (signature->at(0).first) {
             case VT_VOID:
                 compiler.addFunc(kFuncConvHost, FuncBuilder0<Void>());
                 break;
             case VT_DOUBLE:
                 compiler.addFunc(kFuncConvHost, FuncBuilder0<double>());
+                break;
             case VT_INT:
                 compiler.addFunc(kFuncConvHost, FuncBuilder0<signedIntType>());
+                break;
             case VT_STRING:
                 compiler.addFunc(kFuncConvHost, FuncBuilder0<char const *>());
+                break;
             default:
                 throw InterpretationError(string("Wrong NativeFunction return type ") + typeToName(signature->at(0).first));
         }
 
         // ...
-
+        switch (signature->at(0).first) {
+            case VT_DOUBLE: {
+                X86XmmVar retVariable(compiler, kX86VarTypeXmmSd, "returnVariable");
+                // TODO
+                // callNativeFunction->setRet(0, retVariable);
+                compiler.ret(retVariable);
+                break;
+            }
+            case VT_STRING:
+            case VT_INT: {
+                X86GpVar retVariable(compiler, kVarTypeInt64, "returnVariable");
+                // TODO
+                // callNativeFunction->setRet(0, retVariable);
+                compiler.ret(retVariable);
+                break;
+            }
+            case VT_VOID:
+                break;
+            default:
+                throw InterpretationError(string("Wrong NativeFunction return type ") + typeToName(signature->at(0).first));
+        }
+//        compiler.ret(returnVariable);
         compiler.endFunc();
 
-        // call
+        // calling native function
         void *wrappedNativeFunction = compiler.make();
-
         switch (signature->at(0).first) {
             case VT_VOID:
                 asmjit_cast<void (*)()>(wrappedNativeFunction)();
                 break;
             case VT_DOUBLE:
                 pushVariable(asmjit_cast<double (*)()>(wrappedNativeFunction)());
+                break;
             case VT_INT:
                 pushVariable(asmjit_cast<signedIntType(*)()>(wrappedNativeFunction)());
+                break;
             case VT_STRING:
                 pushVariable(asmjit_cast<char const *(*)()>(wrappedNativeFunction)());
+                break;
             default:
                 throw InterpretationError(string("Wrong NativeFunction return type ") + typeToName(signature->at(0).first));
         }
