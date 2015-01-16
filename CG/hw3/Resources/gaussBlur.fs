d07971649baa80c6a472b8bcd67a96b9ba87c7dd
Timur <doxxerACM@gmail.com>
2015-01-16 18:08:50 +0300

    cg hw3

--- CG/hw3/Resources/gaussBlur.fs
+++ CG/hw3/Resources/gaussBlur.fs
@@ -5,7 +5,21 @@ out vec4 result_color;
 
 uniform sampler2D frameTexture;
 
-void main()
-{
-	result_color = vec4(0.6, 0.2, 0.1, 1);
+uniform float weight[5] = float[](0.2270270270, 0.1945945946, 0.1216216216, 0.0540540541, 0.0162162162);
+
+void main() {
+    vec4 result = vec4(0);
+
+    result += texture(frameTexture, uv) * weight[0];
+    for (int i = 1; i < 5; i++) {
+        result += texture(frameTexture, uv + vec2(i, 0.0)/700.0) * weight[i];
+        result += texture(frameTexture, uv - vec2(i, 0.0)/700.0) * weight[i];
+    }
+    result += texture(frameTexture, uv) * weight[0];
+    for (int i = 1; i < 5; i++) {
+    	result += texture(frameTexture, uv + vec2(0.0, i)/350.0) * weight[i];
+    	result += texture(frameTexture, uv - vec2(0.0, i)/350.0) * weight[i];
+    }
+
+    result_color = result / 2.0;
 }
